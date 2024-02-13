e72e26be0d2fb796fa1b1732e2221adff2bcd94d
MUKILTEO\1717834 <mcklammt@gmail.com>
2024-02-13 11:10:16 -0800

    init

---
+++ VectorApp/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VectorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black);
+
+            e.Graphics.DrawLine(pen, 282.5f, 0, 282.5f, 506);
+
+            e.Graphics.DrawLine(pen, 0, 253, 565, 253);
+
+            for (int x = 0; x < 45; x++)
+            {
+                e.Graphics.FillEllipse(pen.Brush, new RectangleF(x * 15 + 1.5f, 250, 6, 6));
+            }
+
+            for (int y = 0; y < 45; y++)
+            {
+                e.Graphics.FillEllipse(pen.Brush, new RectangleF(279, y * 15 + 1.5f, 6, 6));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case (int)calculation.Magnitude:
+                    calculateMagnitude();
+                    break;
+                case (int)calculation.Direction:
+                    calculateDirection();
+                    break;
+                case (int)calculation.Add:
+                    calculateAddition();
+                    break;
+                case (int)calculation.Negation:
+                    calculateNegation();
+                    break;
+                case (int)calculation.Subtraction:
+                    calculateSubtraction();
+                    break;
+                case (int)calculation.Scaling:
+                    calculateScaling();
+                    break;
+                case (int)calculation.Normalization:
+                    calculateNormalization();
+                    break;
+                case (int)calculation.Dot:
+                    calculateDotProduct();
+                    break;
+                case (int)calculation.Cross:
+                    calculateCrossProduct();
+                    break;
+                case (int)calculation.AngleBetween:
+                    calculateAngleBetween();
+                    break;
+                case (int)calculation.Projection:
+                    calculateProjection();
+                    break;
+                default:
+                    break;
+            }
+            
+
+        }
+        
+        private void calculateMagnitude()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            label8.Text = "Answer: " + vecA.magnitude();
+        }
+        private void calculateDirection()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+
+            label8.Text = "Answer: " + vecA.direction() + "°";
+
+        }
+        private void calculateAddition()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + (vecA + vecB).print();
+        }
+        private void calculateNegation()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + vecA.negate().print();
+        }
+        private void calculateSubtraction()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + (vecA - vecB).print();
+        }
+        private void calculateScaling()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + (vecA * vecB).print();
+            
+        }
+        private void calculateNormalization()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + vecA.normalize().print();
+
+        }
+        private void calculateDotProduct()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + vecA.dot(vecB);
+        }
+        private void calculateCrossProduct()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + vecA.cross(vecB);
+        }
+        private void calculateAngleBetween()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+
+            label8.Text = "Answer: " + vecA.angleBetween(vecB) + "°";
+
+        }
+        private void calculateProjection()
+        {
+            Vector2 vecA = new Vector2(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+            Vector2 vecB = new Vector2(float.Parse(textBox4.Text), float.Parse(textBox3.Text));
+            label8.Text = "Answer: " + vecA.projection(vecB).print();
+
+        }
+
+
+        private enum calculation
+        {
+            Magnitude, Direction, Add, Negation, Subtraction, Scaling, Normalization, Dot, Cross, AngleBetween, Projection
+        }
+
+    }
+}
---
+++ VectorApp/Vector2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorApp
+{
+    public class Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2(float x1, float y1)
+        {
+            x = x1;
+            y = y1;
+        }
+
+        public Vector2(double x1, double y1)
+        {
+            x = (float)x1;
+            y = (float)y1;
+        }
+
+
+        public double resulant()
+        {
+            return Convert.ToDouble(x + y);
+        }
+
+        public double resulant(double pow)
+        {
+            return Math.Pow(Convert.ToDouble(x), pow) + Math.Pow(Convert.ToDouble(y), pow);
+        }
+
+        public Vector2 normalize()
+        {
+            double mag = magnitude();
+
+            return new Vector2(x / mag, y / mag);
+        }
+
+        public double dot(Vector2 vec2)
+        {
+            return x * vec2.x + y * vec2.y;
+        }
+
+        public double cross(Vector2 vec2)
+        {
+            return (x * vec2.y) - (y * vec2.x);
+        }
+
+        public double angleBetween(Vector2 vec2)
+        {
+            Vector2 normA = this.normalize();
+            Vector2 normB = vec2.normalize();
+
+            return Math.Acos(normA.dot(normB)) * 180 / Math.PI;
+        }
+
+        public double magnitude()
+        {
+            return Math.Sqrt(Convert.ToDouble(resulant(2)));
+        }
+
+        public double direction()
+        {
+            return Math.Atan(Convert.ToDouble(y / x)) * 180 / Math.PI;
+        }
+
+        public Vector2 projection(Vector2 vecB)
+        {
+            return (vecB * this) / (vecB * vecB) * vecB;
+        }
+
+        public Vector2 negate()
+        {
+            return new Vector2(-x, -y);
+        }
+
+        public string print()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+
+        public static Vector2 operator +(Vector2 veca, Vector2 vecb)
+        {
+            return new Vector2(veca.x + vecb.x, veca.y + vecb.y);
+        }
+        public static Vector2 operator -(Vector2 veca, Vector2 vecb)
+        {
+            return new Vector2(veca.x - vecb.x, veca.y - vecb.y);
+        }
+        public static Vector2 operator *(Vector2 veca, Vector2 vecb)
+        {
+            return new Vector2(veca.x * vecb.x, veca.y * vecb.y);
+        }
+        public static Vector2 operator /(Vector2 veca, Vector2 vecb)
+        {
+            return new Vector2(veca.x / vecb.x, veca.y / vecb.y);
+        }
+        public static Vector2 operator ^(Vector2 veca, double pow)
+        {
+            return new Vector2((float)(Math.Pow(veca.x, pow)), (float)(Math.Pow(veca.y, pow)));
+        }
+    }
+}
